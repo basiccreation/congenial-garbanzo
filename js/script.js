fda77330026b984d9669aeead8f15c73e660e3f1
pia smith <basiccreation@yahoo.com>
2017-07-08 19:03:17 -0700

    set up JSON call to NYT API

--- js/script.js
+++ js/script.js
@@ -1,28 +1,45 @@
-
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+    var $body = $("body");
+    var $wikiElem = $("#wikipedia-links");
+    var $nytHeaderElem = $("#nytimes-header");
+    var $nytElem = $("#nytimes-articles");
+    var $greeting = $("#greeting");
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview image
+    // load Google Street View image
     var streetInput = $("#street").val();
     var cityInput = $("#city").val();
-    var gkey = "AIzaSyBEZIIjRbO6D1Z5rUndEO2aD29JBoM5B2w";
+    var gkey = "74e83c0fbef64668ab64f1bd10492c4e";
     var beginningStreetUrl = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=";
     var picUrl = beginningStreetUrl + streetInput + "," + cityInput + "&" + gkey;
-    $("#street").val('');
-    $("#city").val('');
-    //$( "h1" ).text( picUrl );
-    $body.append('<img class="bgimg" src="' + picUrl + '">');
 
+    $body.append("<img class='bgimg' src='" + picUrl + "'>");
+
+
+    // load New York Times articles
+    var nytkey = "74e83c0fbef64668ab64f1bd10492c4e";
+    var nytSearchTerm = (cityInput).replace(" ", "+");
+    var nytUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + nytSearchTerm + "&sort=newest&&api-key=" + nytkey;
+
+    $.getJSON(nytUrl, function(data) {
+    var response = data.response.docs;
+
+//     $nytHeaderElem.text("New York Times Articles About " + cityInput);
+//            var article = articles[i];
+        //     for (var i = 0 ; i < articles.length ; i++) {
+        //         $nytElem.append("<li class='article'>" + "<a href='" + article.web_url + "'>" + article.headline.main + "</a>" + "<p>" + article.snippet + "</p>" + "</li>");
+        //     };
+        // });
+
+$("#street").val("");
+$("#city").val("");
+$("#details").html(JSON.stringify(response, null, 4));
+});
+//dont touch anything below --------------------
     return false;
 };
-
-$('#form-container').submit(loadData);
+$("#form-container").submit(loadData);
